d2f15256d71c1a37006ce4b4eeb826d40f55ea41
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2018-03-10 17:02:44 +0900

    fiが使えなくなっていたので修正．多重larを一応出来るように修正

--- ata2003lk.lua
+++ ata2003lk.lua
@@ -1,36 +1,44 @@
-local mode = ""
+local mode = {}
+local larstack = {}
 local larcount = 0
 local infile, outfile, err
 
 local function write(str, ws)
 	if str == "zali" then
-		mode = 1
+		table.insert(mode, 1)
 		return outfile:write("nta 4 f5 krz ")
 	elseif str == "ycax" then
-		mode = 0
 		return outfile:write("ata 4 f5" .. ws)
 	elseif str == "fenx" then
-		mode = 2
+		table.insert(mode, 2)
 		return outfile:write("nta 4 f5 inj ")
 	elseif str == "cers" then
-		mode = 0
 		return outfile:write("nll ")
 	elseif str == "dosn" then
-		mode = 0
 		return outfile:write("krz f5@ xx" .. ws)
 	elseif str == "lar" then
-		mode = 0
+		table.insert(mode, 3)
+
 		larcount = larcount + 1
+		table.insert(larstack, larcount)
 		return outfile:write("nll lar" .. tostring(larcount) .. " fi" .. ws)
 	elseif str == "xtlo" or str == "xtlonys" or str == "xylo" or str == "xylonys"
 		or str == "clo" or str == "niv"
 		or str == "llo" or str == "llonys" or str == "xolo" or str == "xolonys" then
-		mode = 0
-		return outfile:write(str .. " malkrz lar-sit" .. tostring(larcount) .. " xx" .. ws)
+		if #mode > 0 and mode[#mode] == 3 then
+			table.remove(mode)
+			return outfile:write(str .. " malkrz lar-sit" .. tostring(larstack[#larstack]) .. " xx" .. ws)
+		else
+			return outfile:write(str .. ws)
+		end
 	elseif str == "ral" then
-		mode = 0
-		return outfile:write("krz lar" .. tostring(larcount) .. " xx\n"
-			.. "nll lar-sit" .. tostring(larcount) .." fen " .. ws)
+		if #larstack == 0 then
+			return nil, "Mismatch: '" .. str .. "'"
+		end
+
+		local count = table.remove(larstack)
+		return outfile:write("krz lar" .. tostring(count) .. " xx\n"
+			.. "nll lar-sit" .. tostring(count) .." fen " .. ws)
 	elseif str ~= "" then
 		local v = str
 
@@ -39,14 +47,16 @@ local function write(str, ws)
 			v = "f5+" .. usestack .. "@"
 		end
 
-		if mode == 1 then
+		local m = table.remove(mode)
+		if m == 1 then
 			v = v .. " f5@" .. ws
-		elseif mode == 2 then
+		elseif m == 2 then
 			v = v .. " xx f5@ ata 4 f5" .. ws
 		else
+			table.insert(mode, m)
 			v = v .. ws
 		end
-		mode = 0
+
 		return outfile:write(v)
 	else
 		return outfile, nil
@@ -82,6 +92,16 @@ function transpile(filename)
 				outfile, err = write(buf, c)
 				if outfile == nil then
 					print("error (transpile) : " .. tostring(err))
+					if #mode == 0 then
+						print("mode stack: ")
+					else
+						print("mode stack: " .. table.concat(mode, ","));
+					end
+					if #larstack == 0 then
+						print("lar stack: ")
+					else
+						print("lar stack: " .. table.concat(larstack, ","));
+					end
 					os.exit(1)
 				end
 
