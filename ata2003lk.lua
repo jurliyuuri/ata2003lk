eb22ea4016116b64017acb715e19850347f9dc52
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2018-11-24 14:26:20 +0900

    ファイル分割と字句解析処理のミスを修正

--- ata2003lk.lua
+++ ata2003lk.lua
@@ -37,9 +37,6 @@ function readfile(filename)
 
             if c == " " or c == "\r" or c == "\n" then
                 if string.match(buf, "[+|]$") ~= nil then
-                    if ~isregister(str) and tonumber(str) == nil then
-                        error("Invalid operator '" .. c .. "'")
-                    end
                     -- 何もしない
                 elseif buf ~= "" then
                     table.insert(words, buf)
@@ -208,7 +205,7 @@ function analyze(words)
     }
 end
 
-local function getlabel(label, outlabel, inlabel)
+function getlabel(label, outlabel, inlabel)
     local isaddress = string.match(label, "[@]$")
     if isaddress then
         local labelpart = string.sub(label, 1, string.len(label) - 1)
@@ -236,7 +233,7 @@ local function getlabel(label, outlabel, inlabel)
     return label
 end
 
-local function getvarlabel(var, outlabel, inlabel)
+function getvarlabel(var, outlabel, inlabel)
     local first, opd, second = string.match(var, "^(%w+)([+|])(%w+)@$")
     local success, label = pcall(getlabel, var, outlabel, inlabel)
 
@@ -312,215 +309,3 @@ local function getvarlabel(var, outlabel, inlabel)
         end
     end
 end
-
-function transpile(analyzed)
-    local i = 1
-    local tokens = analyzed.tokens
-    local count = #tokens
-    local result = {}
-    local larlabel = {}
-    local larsitlabel = {}
-
-    while i <= count do
-        local token = tokens[i]
-
-        if token.operator == "zali" then
-            table.insert(result, {
-                operator = "nta",
-                operands = { "4", "f5" },
-            })
-            table.insert(result, {
-                operator = "krz",
-                operands = {
-                    getvarlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    "f5@",
-                },
-            })
-        elseif token.operator == "ycax" then
-            local num = tonumber(token.operands[1])
-
-            if num ~= nil then
-                table.insert(result, {
-                    operator = "ata",
-                    operands = {
-                        num * 4,
-                        "f5",
-                    },
-                })
-            else
-                table.insert(result, {
-                    operator = "dro",
-                    operands = {
-                        "2",
-                        getvarlabel(token.operands[1]),
-                    },
-                })
-                table.insert(result, {
-                    operator = "ata",
-                    operands = {
-                        getvarlabel(token.operands[1]),
-                        "f5",
-                    },
-                })
-            end
-        elseif token.operator == "fenx" then
-            table.insert(result, {
-                operator = "nta",
-                operands = { "4", "f5" },
-            })
-            table.insert(result, {
-                operator = "inj",
-                operands = {
-                    getlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    "xx",
-                    "f5@"
-                },
-            })
-            table.insert(result, {
-                operator = "ata",
-                operands = { "4", "f5" },
-            })
-        elseif token.operator == "cers" then
-            table.insert(result, {
-                operator = "nll",
-                operands = {
-                    getlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel)
-                },
-            })
-        elseif token.operator == "dosn" then
-            table.insert(result, {
-                operator = "krz",
-                operands = { "f5@", "xx" }
-            })
-        elseif token.operator == "dus" then
-            table.insert(result, {
-                operator = "krz",
-                operands = {
-                    getvarlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    "xx",
-                }
-            })
-        elseif token.operator == "maldus" then
-            table.insert(result, {
-                operator = "malkrz",
-                operands = {
-                    getvarlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    "xx",
-                }
-            })
-        elseif token.operator == "lar" then
-            table.insert(larlabel, "--lar--".. i)
-            table.insert(result, {
-                operator = "nll",
-                operands = { larlabel[#larlabel] },
-            })
-            table.insert(result, {
-                operator = "fi",
-                suboperator = token.suboperator,
-                operands = {
-                    getvarlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    getvarlabel(token.operands[2], analyzed.outlabel, analyzed.inlabel),
-                }
-            })
-            table.insert(larsitlabel, "--lar--sit--" .. i)
-            table.insert(result, {
-                operator = "malkrz",
-                operands = { larsitlabel[#larlabel], "xx" },
-            })
-        elseif token.operator == "ral" then
-            table.insert(result, {
-                operator = "krz",
-                operands = { table.remove(larlabel), "xx" }
-            })
-            table.insert(result, {
-                operator = "nll",
-                operands = { table.remove(larsitlabel) }
-            })
-            if i == count then
-                table.insert(result, {
-                    operator = "fen",
-                    operands = {},
-                })
-            end
-        elseif token.operator == "nll" then
-            table.insert(result, {
-                operator = "nll",
-                operands = {
-                    getlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel)
-                },
-            })
-        elseif token.operator == "l'" then
-            local value = {
-                operator = "l'",
-                operands = {
-                    getlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel)
-                },
-            };
-
-            if analyzed.tokens[i - 1] ~= nil then
-                local prev = analyzed.tokens[i - 1].operator
-                if prev == "zali" or prev == "lar" or prev == "ral" then
-                        table.insert(result, #result, value)
-                elseif prev == "fenx" then
-                    table.insert(result, #result - 1, value)
-                elseif prev == "ycax" and type(result[#result].operands[0]) ~= "number" then
-                    table.insert(result, #result,  value)
-                else
-                    table.insert(result, value)
-                end
-            else
-                error("Nothing operator before \"l'\"")
-            end
-        elseif token.operator == "fi" then
-            table.insert(result, {
-                operator = "fi",
-                suboperator = token.suboperator,
-                operands = {
-                    getvarlabel(token.operands[1], analyzed.outlabel, analyzed.inlabel),
-                    getvarlabel(token.operands[2], analyzed.outlabel, analyzed.inlabel),
-                },
-            })
-        else
-            local operands = {}
-            for i, v in ipairs(token.operands) do
-                table.insert(operands, getvarlabel(v, analyzed.outlabel, analyzed.inlabel))
-            end
-
-            table.insert(result, {
-                operator = token.operator,
-                operands = operands,
-            })
-        end
-
-        i = i + 1
-    end
-
-    local outfile, err = io.open(string.match(arg[1], "(.+)%.alk") .. ".lk", "w")
-    if outfile == nil then
-        print("error (outfile): " .. tostring(err))
-        os.exit(1)
-    end
-
-    outfile:write("'i'c\n")
-    for i, v in ipairs(result) do
-        if v.suboperator ~= nil then
-            outfile:write(v.operator .. " " .. table.concat(v.operands, " ") .. " " .. v.suboperator .. "\n")
-        else
-            outfile:write(v.operator .. " " .. table.concat(v.operands, " ") .. "\n")
-        end
-    end
-end
-
-local wordlist = readfile(arg[1])
-local analyzelist = analyze(wordlist)
--- 確認用
-print(analyzelist)
-for i, v in ipairs(analyzelist.tokens) do
-    if v.suboperator ~= nil then
-        print(i, v.operator .. " " .. table.concat(v.operands, " ") .. " " .. v.suboperator)
-    else
-        print(i, v.operator .. " " .. table.concat(v.operands, " "))
-    end
-end
-
-transpile(analyzelist)
